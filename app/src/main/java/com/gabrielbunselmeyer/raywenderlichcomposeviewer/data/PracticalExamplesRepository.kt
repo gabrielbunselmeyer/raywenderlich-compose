1d757ffb6460815f2e9a65e9c31fadb5793e03c0
Gabriel Bunselmeyer <gabrielbunselmeyer@gmail.com>
2022-01-16 19:20:26 -0300

    Comment out GitHubAuthIntereceptor

--- app/src/main/java/com/gabrielbunselmeyer/raywenderlichcomposeviewer/data/PracticalExamplesRepository.kt
+++ app/src/main/java/com/gabrielbunselmeyer/raywenderlichcomposeviewer/data/PracticalExamplesRepository.kt
@@ -13,7 +13,8 @@ import retrofit2.http.GET
 class PracticalExamplesRepository {
     private val gitHubApi = Retrofit.Builder()
         .baseUrl("https://api.github.com/repos/raywenderlich/ios-interview/contents/Practical%20Example/")
-        .client(OkHttpClient.Builder().addInterceptor(GitHubAuthInterceptor()).build())
+        .client(OkHttpClient())
+//        .client(OkHttpClient.Builder().addInterceptor(GitHubAuthInterceptor()).build())
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(PracticalExamplesApi::class.java)
@@ -24,25 +25,33 @@ class PracticalExamplesRepository {
     suspend fun getVideos(): TutorialContentModel =
         gitHubApi.getVideosFileModel().convertContentToTutorialModel()
 
-    // The GitHubAPI has a relatively low rate limit if you're not authenticated.
-    // This interceptor uses simple authentication through passing in my GitHub user. No PW needed.
-    private class GitHubAuthInterceptor : Interceptor {
-        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
-            val request = chain.request()
-            val authenticatedRequest = request
-                .newBuilder()
-                .header(
-                    "Authorization",
-                    Credentials.basic(
-                        "gabrielbunselmeyer",
-                        "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"
-                    )
-                )
-                .build()
-
-            return chain.proceed(authenticatedRequest)
-        }
-    }
+    /**
+     * The GitHubAPI has a relatively low rate limit if you're not authenticated; 60 requests per hour I believe.
+     * This interceptor can be used to authenticate through GitHub. It's commented out as it might not be needed
+     * if you won't do any heavy testing.
+     * To authenticate, you can use your username and access token, as detailed here:
+     * https://docs.github.com/en/rest/guides/getting-started-with-the-rest-api#authentication
+     * For security reasons I'm not leaving my own access token in and have since deleted it.
+     *
+     * You'll also need to uncomment the [OkHttpClient] call in like 17 (in the Retrofit Builder).
+     */
+//    private class GitHubAuthInterceptor : Interceptor {
+//        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+//            val request = chain.request()
+//            val authenticatedRequest = request
+//                .newBuilder()
+//                .header(
+//                    "Authorization",
+//                    Credentials.basic(
+//                        "gabrielbunselmeyer",
+//                        ACCESS_TOKEN
+//                    )
+//                )
+//                .build()
+//
+//            return chain.proceed(authenticatedRequest)
+//        }
+//    }
 }
 
 interface PracticalExamplesApi {
